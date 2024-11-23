52d17bbc592d24a10edda813732180de1204c28b
samirjabib <samirdev9669@gmail.com>
2024-11-22 19:35:19 -0500

    more imports fixs

--- app/components/product/product-quick-view/types.ts
+++ app/components/product/product-quick-view/types.ts
@@ -1,56 +1,56 @@
 import { CollectionProductFragment } from "storefrontapi.generated";
-import { Variant } from "../../../product/product-card/ProductCard";
+import { Variant } from "~/components/design-system/product-card/ProductCard";
 
 export type ProductsQuickViewProps = {
-    variant: Variant;
-    product: CollectionProductFragment;
+  variant: Variant;
+  product: CollectionProductFragment;
+};
+
+
+export type QuantityCellProps = {
+  quantity: number;
+  onQuantityChange: (newQuantity: number) => void;
+  isMinQuantity: boolean;
+  isMaxQuantity: boolean;
+};
+
+
+export type VariantWithMetaField = CollectionProductFragment['variants']['nodes'][0] & {
+  metafield: {
+    key: string;
+    value: string;
   };
+};
 
 
-  export type QuantityCellProps = {
-    quantity: number;
-    onQuantityChange: (newQuantity: number) => void;
-    isMinQuantity: boolean;
-    isMaxQuantity: boolean;
-  };
-  
+export type ProductVariant = {
+  name: string;
+  price: string;
+  discount: string;
+  total: string;
+  size: string;
+}
 
-  export type VariantWithMetaField = CollectionProductFragment['variants']['nodes'][0] & {
-    metafield: {
-      key: string;
-      value: string;
-    };
-  };
+export type ProductVariantTableProps = {
+  variants: CollectionProductFragment['variants']['nodes'];
+}
+
+export type ProductHeaderProps = {
+  title: string;
+  subtitle?: string;
+  rating?: number;
+  reviewCount?: number;
+  tags?: string[];
+  imageSrc?: string;
+  imageAlt?: string;
+}
+
+export type TableRowProps = {
+  variant: VariantWithMetaField;
+};
 
 
-  export type ProductVariant  = {
-    name: string;
-    price: string;
-    discount: string;
-    total: string;
-    size: string;
-  }
-  
-  export type ProductVariantTableProps =  {
-    variants:CollectionProductFragment['variants']['nodes'] ;
-  }
-
-  export type ProductHeaderProps =  {
-    title: string;
-    subtitle?: string;
-    rating?: number;
-    reviewCount?: number;
-    tags?: string[];
-    imageSrc?: string;
-    imageAlt?: string;
-  }
-
-  export type TableRowProps = {
-    variant: VariantWithMetaField;
-  };
-  
-  
-  export type CartSummaryProps =   {
-    totalItems: number;
-    subtotal: number;
-  };
+export type CartSummaryProps = {
+  totalItems: number;
+  subtotal: number;
+};
