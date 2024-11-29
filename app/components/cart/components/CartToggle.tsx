ac7c5a88beae95c423d744ecbdbf19692656b8c6
samirjabib <samirdev9669@gmail.com>
2024-11-28 23:11:23 -0500

    remove supense ,try with useEffect

--- app/components/cart/components/CartToggle.tsx
+++ app/components/cart/components/CartToggle.tsx
@@ -1,6 +1,5 @@
-import { Await, useAsyncValue } from '@remix-run/react';
+import { useState, useEffect } from 'react';
 import { CartBadge } from './CartBadge';
-import { Suspense } from 'react';
 import type { CartToggleProps } from '../types';
 import { ShoppingBag } from 'lucide-react';
 import type { CartApiQueryFragment } from 'storefrontapi.generated';
@@ -9,35 +8,43 @@ import { Icon } from '../../ui/Icon';
 import { useCartStore } from './cartStore';
 
 export function CartToggle({ cart }: CartToggleProps) {
-  const originalCart = useAsyncValue() as CartApiQueryFragment | null;
-  const cartUpdated = useOptimisticCart(originalCart);
+  const [resolvedCart, setResolvedCart] = useState<CartApiQueryFragment | null>(null);
   const open = useCartStore((set) => set.open);
   const { publish, shop, cart: cartAnalytics, prevCart } = useAnalytics();
 
+  // Cargar el carrito de forma asíncrona con useEffect
+  useEffect(() => {
+    const fetchCart = async () => {
+      if (cart) {
+        const fetchedCart = await cart;
+        setResolvedCart(fetchedCart);
+      }
+    };
+
+    fetchCart();
+  }, [cart])
+
+  if (!resolvedCart) {
+    return <div>Loading...</div>; // o un skeleton loader si prefieres
+  }
 
   return (
-    <Suspense fallback={<div>error de hydratacion</div>}>
-      <Await resolve={cart}>
-        {(resolvedCart) => (
-          <button
-            aria-label="Open cart"
-            className="relative cursor-pointer transition-all hover:bg-gray-200 rounded-lg"
-            onClick={(e) => {
-              e.preventDefault();
-              open();
-              publish('cart_viewed', {
-                cart: cartAnalytics,
-                prevCart,
-                shop,
-                url: window.location.href || '',
-              });
-            }}
-          >
-            <CartBadge count={resolvedCart?.totalQuantity ?? cartUpdated?.totalQuantity ?? 0} />
-            <Icon name="bag" size={30} />
-          </button>
-        )}
-      </Await>
-    </Suspense>
+    <button
+      aria-label="Open cart"
+      className="relative cursor-pointer transition-all hover:bg-gray-200 rounded-lg"
+      onClick={(e) => {
+        e.preventDefault();
+        open();
+        publish('cart_viewed', {
+          cart: cartAnalytics,
+          prevCart,
+          shop,
+          url: window.location.href || '',
+        });
+      }}
+    >
+      <CartBadge count={resolvedCart?.totalQuantity ?? 0} />
+      <Icon name="bag" size={30} />
+    </button>
   );
 }
